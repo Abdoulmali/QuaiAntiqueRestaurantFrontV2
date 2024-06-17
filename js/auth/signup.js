6dde4c7c318f0f127e4b975d3b2f62b2f24e9ec1
Abdoulmali <137415411+Abdoulmali@users.noreply.github.com>
2024-06-17 22:48:58 +0200

    form signup finish

--- js/auth/signup.js
+++ js/auth/signup.js
@@ -19,12 +19,26 @@ function validateForm(){
   const prenomOK = validateRequired(InputPrenom);
   const mailOk = validateMail(InputEmail);
   const passwordOk = ValidatePassword(InputPassword);
+  const passwordConfirmOk = validateConfirmationPassword(InputPassword, InputValidatePassword);
 
-  if(nomOK && prenomOK && mailOk && passwordOk){
+  if(nomOK && prenomOK && mailOk && passwordOk && passwordConfirmOk){
     btnValidation.disabled = false;
   }
   else{
     btnValidation.disabled = true;
+  }
+}
+
+function validateConfirmationPassword(InputPwd, inputConfirmPwd){
+  if(InputPassword.value == inputConfirmPwd.value){
+    inputConfirmPwd.classList.add("is-valid");
+    inputConfirmPwd.classList.remove("is-invalid");
+    return true;
+  }
+  else{
+    inputConfirmPwd.classList.add("is-invalid");
+    inputConfirmPwd.classList.remove("is-valid");
+    return false;
   }
 }
 
