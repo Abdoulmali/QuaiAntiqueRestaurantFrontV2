9be6de689c70fd85093d0c143c6cea7bd540d149
Abdoulmali <137415411+Abdoulmali@users.noreply.github.com>
2024-06-17 22:36:30 +0200

    Ajout champ form

--- js/auth/signup.js
+++ js/auth/signup.js
@@ -18,8 +18,9 @@ function validateForm(){
   const nomOK = validateRequired(inputNom);
   const prenomOK = validateRequired(InputPrenom);
   const mailOk = validateMail(InputEmail);
+  const passwordOk = ValidatePassword(InputPassword);
 
-  if(nomOK && prenomOK && mailOk){
+  if(nomOK && prenomOK && mailOk && passwordOk){
     btnValidation.disabled = false;
   }
   else{
@@ -27,8 +28,24 @@ function validateForm(){
   }
 }
 
+function ValidatePassword(input){
+  //Définir mon regex password
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,}$/;
+  const passwordUser = input.value;
+  if(passwordUser.match(passwordRegex)){
+    input.classList.add("is-valid");
+    input.classList.remove("is-invalid");
+    return true;
+  }
+  else{
+    input.classList.remove("is-valid");
+    input.classList.add("is-invalid");
+    return false;
+  }
+}
+
 function validateMail(input){
-  //Définir mon regex
+  //Définir mon regex mail
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   const mailUser = input.value;
   if(mailUser.match(emailRegex)){
