6796a8284c52cec3a8e77e9f0fcb0de2e9686acd
Abdoulmali <137415411+Abdoulmali@users.noreply.github.com>
2024-07-03 20:20:14 +0200

    Finish connexion

--- js/auth/signin.js
+++ js/auth/signin.js
@@ -1,28 +1,44 @@
 const mailInput = document.getElementById("EmailInput");
 const passwordInput = document.getElementById("PasswordInput");
 const btnSignin = document.getElementById("btnSignin");
+const signinForm = document.getElementById("signinForm");
 
 btnSignin.addEventListener("click", checkCredentials);
 
-function checkCredentials(){
-  //Ici il faudra appeler l'API pour vérifier les credentials en BDD
-
-  if(mailInput.value == "test@mail.com" && passwordInput.value == "123"){
-    
-    
-    // Il faudra récupérer le vrai token 
-const token = "bdzhdvjvvhvhjlioiuudaupvvvjhjnlkjl";
-setToken(tokenCookieName);
+function checkCredentials() {
+  let dataForm = new FormData(signinForm);
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  
+  const raw = JSON.stringify({
+    "username": dataForm.get("email"),
+    "password": dataForm.get("mdp")
+  });
+  
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow"
+  };
+  
+  fetch(apiUrl+"login", requestOptions)
+    .then((response) => {
+      if(response.ok){
+        return response.json();
+      }
+      else{
+        mailInput.classList.add("is-invalid");
+    passwordInput.classList.add("is-invalid");
+      }
+    })
+    .then((result) => {
+    const token = result.apiToken;
+    setToken(token);
     // placer ce token en cookies
 
-    setCookie(RoleCookieName, "client", 7);
+    setCookie(RoleCookieName, result.roles[0], 7);
     window.location.replace("/");
-
-  }
-  else{
-    mailInput.classList.add("is-invalid");
-    passwordInput.classList.add("is-invalid");
-
-    alert("Vous n'etes pas connecté");
-  }
+    })
+    .catch((error) => console.error(error));
 }
